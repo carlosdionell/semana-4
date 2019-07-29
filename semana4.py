ec69921b0193f0411c12d972cba18399f25a9a0a
carlosdionell <37004879+carlosdionell@users.noreply.github.com>
2019-07-29 14:57:01 -0500

    semana4

---
+++ semana4.py
@@ -0,0 +1,31 @@
+import os
+
+# arbol binario valor de cada nodo
+a = 8
+b = 3
+c = 1
+d = 6
+e = 7
+f = 4
+g = 10
+h = 14
+i = 13
+
+numeronodo = int(input("digite un numeronodo:"))
+if numeronodo == 8:
+    print("el numeronodo es raiz del arbol")
+elif numero > 8:
+    print("el numeronodo no es la raiz del arbol")
+
+numeronodo = int(input("digite un numeronodoizquierdo:"))
+if numeronodo == 7:
+    print("el numeronodo es el subarbolizquierdo")
+elif numero > 7:
+    print("el numeronodo no es del subarbolizquierdo")
+
+numeronodo = int(input("digite un numeronododerecho:"))
+if numeronodo == 13:
+    print("el numeronodo es el subarbolderecho")
+elif numero > 13:
+    print("el numeronodo no es del subarbolderecho")
+
